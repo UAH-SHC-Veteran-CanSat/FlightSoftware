0482d52c950f02b7631db160996b305e135e8319
Thomas <trb0023@uah.edu>
2019-06-07 11:39:17 -0700

    Added IMU wrapper files

---
+++ WinSat Probe Code/src/DRIVERS/IMU.c
@@ -0,0 +1,8 @@
+/*
+ * IMU.c
+ *
+ * Created: 6/7/2019 11:22:59 AM
+ *  Author: trbinsc
+ */ 
+
+#include "IMU.h"
---
+++ WinSat Probe Code/src/DRIVERS/IMU.h
@@ -0,0 +1,16 @@
+/*
+ * IMU.h
+ *
+ * Created: 6/7/2019 11:23:19 AM
+ *  Author: trbinsc
+ */ 
+
+
+#ifndef IMU_H_
+#define IMU_H_
+
+
+
+
+
+#endif /* IMU_H_ */
